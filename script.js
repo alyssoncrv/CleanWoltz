7f0f0a3aa5cbd883f46d5ca59482e6c258281733
Alysson Carvalho <alyssonper@gmail.com>
2023-08-11 12:37:38 -0300

    change styles

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function menuShow() {
-  let menuMobile = document.querySelector('.mobile-menu');
-  if (menuMobile.classList.contains('open')) {
-    menuMobile.classList,remove('open');
-    document.querySelector('.icon').src = ''
-  } else {
-    menuMobile.classList.add('open');
-  }
+const toggle = document.getElementById('toggle');
+
+function handleClick() {
+  button.classList.toggle('active');
 }
